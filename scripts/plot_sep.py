97f68d8c493598f5f0933d7c4ad49f7649acc793
amirali <amirali.kaboli@gmail.com>
2024-07-04 09:41:42 +0100

    add mode to plot_sep

--- scripts/plot_sep.py
+++ scripts/plot_sep.py
@@ -18,13 +18,15 @@ if __name__ == '__main__':
 	mode = args[0] if args else "gj"
 
 	if mode == "gj":
-		alt = "gj"
+		our_alt = "our_gj"
+		fj_alt = "gj"
 	else:
-		alt = "fj_scalar_colt"
+		our_alt = "our_fj"
+		fj_alt = "fj_scalar_colt"
 
 	with open(f"{os.path.dirname(__file__)}/../gj/gj.json") as json_file:
 		results = json.load(json_file)
-	queries = [result["query"] for result in results["ours"]]
+	queries = [result["query"] for result in results[our_alt]]
 
 	duckdb_results = []
 	for result in results["duckdb"]:
@@ -33,36 +35,38 @@ if __name__ == '__main__':
 	duckdb_results = sorted(duckdb_results)
 	duckdb_times = [result[1] for result in duckdb_results]
 
-	ours_results = sorted([(result["query"], float(result["time"])) for result in results["ours"]])
-	queries = [result[0] for result in ours_results]
-	ours_times = [result[1] for result in ours_results]
+	our_results = sorted([(result["query"], float(result["time"])) for result in results[our_alt]])
+	queries = [result[0] for result in our_results]
+	our_times = [result[1] for result in our_results]
 
-	gj_results = sorted([(result["query"], statistics.mean(result["time"])) for result in results[alt]])
+	gj_results = sorted([(result["query"], statistics.mean(result["time"])) for result in results[fj_alt]])
 	gj_results = [result for result in gj_results if result[0] in queries]
 	gj_times = [result[1] for result in gj_results]
 
-	print(f'DuckDB Speedup: {geometric_mean(ours_times, duckdb_times)}x')
-	print(f'{mode.upper()} Speedup: {geometric_mean(ours_times, gj_times)}x')
+	print(f'DuckDB Speedup: {geometric_mean(our_times, duckdb_times)}x')
+	print(f'{mode.upper()} Speedup: {geometric_mean(our_times, gj_times)}x')
+	# ratios = np.array(our_times) / np.array(gj_times)
+	# print("Worst query:", queries[np.argmax(ratios)])
 
 	plt.figure(figsize=(12, 5))
 	ratio = 1.2
 
 	plt.subplot(1, 2, 1)
-	plt.scatter(gj_times, ours_times, color="orange", s=10)
-	points = [max(min(gj_times), min(ours_times)) / ratio, min(max(gj_times), max(ours_times)) * ratio]
+	plt.scatter(gj_times, our_times, color="orange", s=10)
+	points = [max(min(gj_times), min(our_times)) / ratio, min(max(gj_times), max(our_times)) * ratio]
 	plt.plot(points, points, color="gray")
 	plt.xscale("log")
 	plt.yscale("log")
 	plt.xlabel(f"{mode.upper()} (s)")
-	plt.ylabel("Ours (s)")
+	plt.ylabel(f"Our {mode.upper()} (s)")
 
 	plt.subplot(1, 2, 2)
-	plt.scatter(duckdb_times, ours_times, color="orange", s=10)
-	points = [max(min(duckdb_times), min(ours_times)) / ratio, min(max(duckdb_times), max(ours_times)) * ratio]
+	plt.scatter(duckdb_times, our_times, color="orange", s=10)
+	points = [max(min(duckdb_times), min(our_times)) / ratio, min(max(duckdb_times), max(our_times)) * ratio]
 	plt.plot(points, points, color="gray")
 	plt.xscale("log")
 	plt.yscale("log")
 	plt.xlabel("DuckDB (s)")
-	plt.ylabel("Ours (s)")
+	plt.ylabel(f"Our {mode.upper()} (s)")
 
 	plt.show()
